379c39da7f17545a101af9170f04bda646126c62
do4best657 <panic_4_what@hotmail.com>
2026-04-04 15:34:49 +0500

    To Do List in Django is in  process and facing some issue in login object

---
+++ mydjango/todoapp/url.py
@@ -0,0 +1,13 @@
+
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+   
+    path('',views.home,name="home"),
+    path('register/',views.register_view,name="register"),
+    path('login/',views.login_view,name="login"),
+    path('logout/',views.logout_view,name="logout"),
+    path('deleted/',views.delete_task,name="deleted"),
+    
+]
--- mydjango/todoapp/views.py
+++ mydjango/todoapp/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,redirect
+from . forms import RegisterForm 
 
 # Create your views here.
+def home(request):
+    return render(request,"index.html")
+
+def register_view(request):
+    form = RegisterForm()
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            form = RegisterForm()
+    return render(request,"register.html",{
+        'form': form
+    })
+
+
+def login_view(request):
+    return render(request,"login.html")
+
+def logout_view(request):
+    return render(request,"logout.html")
+
+def delete_task(request):
+    return HttpResponse("Deleted")
+
