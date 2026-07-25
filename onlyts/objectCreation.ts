a859e9c41c802ade70e01c1ec9314979c93a5270
do4best657 <panic_4_what@hotmail.com>
2026-07-25 17:21:44 +0500

    How Strange despite best effort we are unable to fund the solution for bubble sort in TS.

--- onlyts/objectCreation.ts
+++ onlyts/objectCreation.ts
@@ -19,3 +19,36 @@ const MyArea:Area={
     height:56,
     width:78
 }
+function factorial(num:number):number{
+    if(num === 1) return 1;
+    return num + factorial(num-1)
+}
+function sumup(num:number):number{
+    let sum=0;
+    for(let i=1; i<=num; i++){
+        sum += i;
+    }
+    return sum;
+}
+console.log(sumup(40))
+console.log(factorial(40))
+/////// bubble sort
+const bubbleSort = (arr: number[]): number[] => {
+    const len = arr.length;
+
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (arr[j]! > arr[j + 1]!) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+
+    return arr;
+}
+
+
+const array = [9, 10, 3, 2, 1, 0, 5, 4, 6, 7, 8];
+console.log(bubbleSort(array));
+
+
